ef52ade4a66a3ec7294e87a9b0486f6d2703931f
Sudip Paul <paul.sudip.ind@gmail.com>
2025-01-16 00:38:20 +0530

    .

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "cravings-fc942",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
